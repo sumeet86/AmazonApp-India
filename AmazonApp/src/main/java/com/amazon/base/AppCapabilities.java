ca86476bddd80dc81152cf51ced3f826ff905243
insum <insum@LAPTOP-8EO5ID8Q>
2019-08-02 11:18:34 +0530

    New implemantation

--- AmazonApp/src/main/java/com/amazon/base/AppCapabilities.java
+++ AmazonApp/src/main/java/com/amazon/base/AppCapabilities.java
@@ -5,11 +5,14 @@ import java.net.URL;
 
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServerHasNotBeenStartedLocallyException;
 
 public class AppCapabilities {
 	public static String deviceName = "Galaxy A50";
@@ -19,9 +22,17 @@ public class AppCapabilities {
 	public static String appActivity = "com.amazon.mShop.home.HomeActivity";
 
 	public static AndroidDriver<AndroidElement> driver;
+	private static AppiumDriverLocalService service;
 
-	 @BeforeClass	
+	@BeforeClass
 	public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException {
+
+		service = AppiumDriverLocalService.buildDefaultService();
+		service.start();
+
+		if (service == null || !service.isRunning()) {
+			throw new AppiumServerHasNotBeenStartedLocallyException("An appium server node is not started!");
+		}
 
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(CapabilityType.BROWSER_NAME, "");
@@ -36,6 +47,16 @@ public class AppCapabilities {
 		driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
 		return driver;
 
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		if (driver != null) {
+			driver.quit();
+		}
+		if (service != null) {
+			service.stop();
+		}
 	}
 
 }
